549e460870b0a3d3e64842c7d7cebcb6e1ea2137
Sofiae01 <105079647+Sofiae01@users.noreply.github.com>
2023-12-10 18:16:04 +0100

    Update spolecne.js

---
+++ spolecne.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('#menu-tlacitko')
+
+
+button.addEventListener ('click', function() {
+    const items = document.querySelector('#menu-polozky')
+    if (items.classList.contains('show')) {
+        items.classList.remove('show')
+        button.querySelector('i').classList.remove('fa-xmark')
+        button.querySelector('i').classList.add('fa-bars')
+
+    } else {
+        items.classList.add('show')
+        button.querySelector('i').classList.remove('fa-bars')
+        button.querySelector('i').classList.add('fa-xmark')
+    }
+}
+)
+
+
+
+
